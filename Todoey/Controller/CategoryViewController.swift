f46eb8e9e9d7f2fe4575b7cba8f714ae42525d59
Denim <dagn456@mail.ru>
2023-03-15 17:55:19 +0400

    Глава 6 View Controller Category читает базу данных и добавляет в нее новые объекты

--- Todoey/Controller/CategoryViewController.swift
+++ Todoey/Controller/CategoryViewController.swift
@@ -7,33 +7,99 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
-
-    var categoryArr = [String]()
+    
+    var categoryArr = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext /// В Aple Delegate есть так называем контекст - временная зона в которой мы можем редактировать данные и после этого добавить контекст в контейнер базы данных через эту строчку мы хотим получить ссылку на контекст для взаимодействия с постоянным контейнером
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let navigationBar = self.navigationController?.navigationBar
+        navigationBar?.barStyle = UIBarStyle.black
+        navigationBar?.backgroundColor = UIColor.systemMint
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        categoryArr.append("Помыть голову")
-
-    }
-    
-    
-    @IBAction func addPressed(_ sender: UIBarButtonItem) {
+        loadData()
         
     }
     
+// MARK: - Добавление нового предмета
+    
+    @IBAction func addPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Добавить новую каттегорию", message: "", preferredStyle: .alert) /// Создаем оповещение
+        
+        alert.addTextField { UITextField in  /// Создаем текстовое поле
+            textField = UITextField
+            print("Yeah")
+        }
+        
+        let action = UIAlertAction(title: "Добавить", style: .default) { UIAlertAction in /// Создаем действие после нажатия на кнопку добавить
+            
+            let newCategory = Category(context: self.context)  /// Инициализирует подкласс управляемого объекта и вставляет его в указанный контекст управляемого объекта.
 
-    // MARK: - Table view data source
+            newCategory.name = textField.text!
+            self.categoryArr.append(newCategory) /// Добавляем в наш массив новый элемент
+            self.saveData()
+        }
+        
+        alert.addAction(action) /// Добавляем в наше оповещение действие
+        
+        present(alert, animated: true) /// Презентуем наше оповещение с текстовым полем и действием, наше текствое поле это UIAlertController поэтому нужна презентация как нового view controllerв
+    }
+    
+}
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+
+    // MARK: - Создание ячеек
+    
+    extension CategoryViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categoryArr.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categoryArr[indexPath.row].name
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
+}
 
+//MARK: -  Работа с данными
 
+extension CategoryViewController {
+    
+    func saveData(){
+        do {
+            try context.save()
+        }catch{
+            print("Ошибка сохранения категорий - \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadData(){
+        
+        let request : NSFetchRequest<Category> = Category.fetchRequest() /// Создаем новый запрос на получение всех элементов из контейнера Category
+        
+        do{
+            categoryArr =  try context.fetch(request)
+        }catch{
+            print("Ошибка получения данных категорий - \(error)")
+        }
+        tableView.reloadData()
+    }
+    
 }
